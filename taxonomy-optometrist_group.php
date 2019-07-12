99989bac70117dde8ad3925b93b919ba4508b0ab
Nick Cartwright <dominic.cartwright07@gmail.com>
2019-07-12 12:18:46 -0400

    Add file to overwrite parent template

---
+++ taxonomy-optometrist_group.php
@@ -0,0 +1,22 @@
+<?php
+
+	//Getting Header
+	get_header();
+
+?>
+
+	<!-- Content section -->
+	<div class="our-team module">
+		<div class="row our-staff-page">
+			<?php
+
+				get_template_part('template-parts/post-type/group-optometrist');
+
+			?>
+		</div><!-- Row Ends /-->
+	</div>
+	<!-- Content Section Ends /-->
+
+<?php
+	//Getting Footer
+	get_footer();
