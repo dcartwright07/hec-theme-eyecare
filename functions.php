cdf0bec76f9f533ec2fc2137f3f542d91932c1f1
Nick Cartwright <dominic.cartwright07@gmail.com>
2019-10-25 13:51:42 -0400

    Add facebook sdk in function.php

--- functions.php
+++ functions.php
@@ -6,7 +6,22 @@
  */
 
 function wc_child_scripts() {
-    wp_enqueue_style( 'wc-parent-style', get_template_directory_uri(). '/style.css', array(), WC_VER, 'all');
+
+		wp_enqueue_style( 'wc-parent-style', get_template_directory_uri(). '/style.css', array(), WC_VER, 'all' );
+
 }
 //High Priority Action
-add_action( 'wp_enqueue_scripts', 'wc_child_scripts', 99);
+add_action( 'wp_enqueue_scripts', 'wc_child_scripts', 99 );
+
+function hec_facebook_javascript_sdk() {
+
+	?>
+
+	<div id="fb-root"></div>
+	<script async defer crossorigin="anonymous" src="https://connect.facebook.net/en_US/sdk.js#xfbml=1&version=v4.0"></script>
+
+	<?php
+
+}
+
+add_action( 'wp_body_open', 'hec_facebook_javascript_sdk' );
