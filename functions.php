608fe9caf4f0f110a6f795e201d008bb58c1ebfb
Nick Cartwright <dominic.cartwright07@gmail.com>
2019-10-30 20:48:16 -0400

    Reorganize code

--- functions.php
+++ functions.php
@@ -5,13 +5,15 @@
  * You can Define your Functions in this file if using Child Theme.
  */
 
-function wc_child_scripts() {
+// Actions for functions
+add_action( 'wp_enqueue_scripts', 'hec_child_scripts', 99 ); //High Priority Action
+add_action( 'wc_after_body_start', 'hec_facebook_javascript_sdk' );
 
-		wp_enqueue_style( 'wc-parent-style', get_template_directory_uri(). '/style.css', array(), WC_VER, 'all' );
+function hec_child_scripts() {
+
+	wp_enqueue_style( 'wc-parent-style', get_template_directory_uri(). '/style.css', array(), WC_VER, 'all' );
 
 }
-//High Priority Action
-add_action( 'wp_enqueue_scripts', 'wc_child_scripts', 99 );
 
 // Add Facebook Javascript SDK right after the opening of the body tag for Facebook feed.
 function hec_facebook_javascript_sdk() {
@@ -24,4 +26,3 @@ function hec_facebook_javascript_sdk() {
 	<?php
 
 }
-add_action( 'wc_after_body_start', 'hec_facebook_javascript_sdk' );
